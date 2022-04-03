e51586bd69834ffbd74aa3bcf533eb83d0930c16
Reece Hewitt <reecehewitt22@gmail.com>
2022-04-03 20:19:10 +0100

    Added 2 snippets

--- estore-php-snippets.php
+++ estore-php-snippets.php
@@ -1,1 +1,21 @@
+<?php
 
+/** E-Store Shop - Custom Currency Symbol */
+
+/** Symbol changes based on currency */
+
+add_filter( 'woocommerce_currencies', 'estore_add_my_currency' );
+
+function estore_add_my_currency( $currencies ) {
+     $currencies['ABC'] = __( 'Currency name', 'woocommerce' );
+     return $currencies;
+}
+
+add_filter('woocommerce_currency_symbol', 'estore_add_my_currency_symbol', 10, 2);
+
+function estore_add_my_currency_symbol( $currency_symbol, $currency ) {
+     switch( $currency ) {
+          case 'ABC': $currency_symbol = '$'; break;
+     }
+     return $currency_symbol;
+}
